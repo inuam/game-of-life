92e72f4b806a0acee94bd58ef4e17d757eec0a92
Amin Uddin <amin_uddin@hotmail.com>
2020-01-25 23:51:40 +0000

    Added tests for rule edge cases

--- src/main/java/conway/gol/Rules.java
+++ src/main/java/conway/gol/Rules.java
@@ -6,6 +6,10 @@ import static conway.gol.Cell.Dead;
 public class Rules {
 
     public Cell apply(Cell currentState, int liveNeighbours) {
+
+        if (liveNeighbours > 8)
+            throw new IllegalArgumentException("Cell neighbours can't exceed 8");
+
         if (currentState == Alive && liveNeighbours < 2)
             return Dead;
         if (currentState == Alive && liveNeighbours > 3)
--- src/test/java/conway/gol/RulesTest.java
+++ src/test/java/conway/gol/RulesTest.java
@@ -1,7 +1,7 @@
 package conway.gol;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -20,7 +20,7 @@ class RulesTest {
      */
 
     @DisplayName("Any live cell with fewer than two live neighbours dies, as if by underpopulation.")
-    @ParameterizedTest(name="Live cell with {0} live neighbour dies")
+    @ParameterizedTest(name = "Live cell with {0} live neighbour dies")
     @CsvSource(value = {"0", "1"})
     public void shouldDieGivenALiveCellWith_FewerThanTwoLiveNeighbours(int liveNeighbours) {
         // Given
@@ -35,7 +35,7 @@ class RulesTest {
     }
 
     @DisplayName("Any live cell with two or three live neighbours lives on to the next generation.")
-    @ParameterizedTest(name="Live cell with {0} live neighbour lives")
+    @ParameterizedTest(name = "Live cell with {0} live neighbour lives")
     @CsvSource(value = {"2", "3"})
     public void shouldStayAliveGivenALiveCellWith_TwOrThreeLiveNeighbours(int liveNeighbours) {
         // Given
@@ -50,7 +50,7 @@ class RulesTest {
     }
 
     @DisplayName("Any live cell with more than three live neighbours dies, as if by overpopulation.")
-    @ParameterizedTest(name="Live cell with  more {0} live neighbour dies")
+    @ParameterizedTest(name = "Live cell with  more {0} live neighbour dies")
     @CsvSource(value = {"4", "5", "6", "7", "8"})
     public void shouldDieGivenALiveCellWith_MoreThanThreeLiveNeighbours(int liveNeighbours) {
         // Given
@@ -65,7 +65,7 @@ class RulesTest {
     }
 
     @DisplayName("Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.")
-    @ParameterizedTest(name="Live cell with  more {0} live neighbour dies")
+    @ParameterizedTest(name = "Dead cell with  more {0} live neighbours come alive")
     @CsvSource(value = {"3"})
     public void shouldComeAliveGivenADeadCellWith_ExactlyThreeLiveNeighbours(int liveNeighbours) {
         // Given
@@ -79,4 +79,34 @@ class RulesTest {
                 .isEqualTo(Alive);
     }
 
+    @DisplayName("Any dead cell with exactly not three live neighbours becomes stays dead")
+    @ParameterizedTest(name = "Dead cell with {0} live neighbour stays dead")
+    @CsvSource(value = {"0", "1", "2", "4", "5", "6", "7", "8"})
+    public void shouldStayDeadWithAnyNumberOfLiveNeighboursOtherThanThree(int liveNeighbours) {
+        // Given
+        Rules classToTest = new Rules();
+
+        // When
+        Cell actual = classToTest.apply(Dead, liveNeighbours);
+
+        // Then
+        assertThat(actual)
+                .isEqualTo(Dead);
+    }
+
+    @ParameterizedTest(name = "Any cell with more than {0} cell should thrown illegal argument exception")
+    @CsvSource(value = {"Dead:9", "Alive:9"}, delimiter = ':')
+    public void shouldThrowIllegalArgumentException_GivenACell_WithMoreThanEight_Neighbours(Cell cell, int neighbours) {
+        // Given
+        Rules classToTest = new Rules();
+
+        // when
+        IllegalArgumentException illegalArgumentException = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            classToTest.apply(cell, neighbours);
+            ;
+        });
+
+        // Then
+        assertThat(illegalArgumentException).isInstanceOf(IllegalArgumentException.class);
+    }
 }
